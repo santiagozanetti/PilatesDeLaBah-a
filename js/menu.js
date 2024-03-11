773b494527343bae9d294576820063228b9e4ea7
Santiago <santiango.zanetti@cardenalspionla.com.ar>
2024-03-11 16:01:45 -0300

    agrego funciones de boton y boton flotante de wpp

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,7 @@
   const openButton = document.querySelector(".nav__menu");
   const menu = document.querySelector(".nav__link");
   const closeMenu = document.querySelector(".nav__close");
+  const links = document.querySelectorAll(".nav__links"); // Selecciona todos los enlaces del menú
 
   openButton.addEventListener("click", () => {
     menu.classList.add("nav__link--show");
@@ -9,5 +10,12 @@
 
   closeMenu.addEventListener("click", () => {
     menu.classList.remove("nav__link--show");
+  });
+
+  // Agrega un evento de clic a cada enlace del menú
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("nav__link--show"); // Cierra el menú al hacer clic en un enlace
+    });
   });
 })();
