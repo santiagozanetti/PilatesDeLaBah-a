ed383c8a3d38b8029d37db2b141ed3841c785a79
Santiago <santiango.zanetti@cardenalspionla.com.ar>
2024-03-13 18:26:28 -0300

    agrego subject y body del mail

--- js/mail.js
+++ js/mail.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const emailButton = document.getElementById("emailButton");
   const emailAddress = "pilatesdelabahia@gmail.com";
+  const subject = encodeURIComponent("Consulta sobre Pilates");
+  const body = encodeURIComponent(
+    "Hola,\n\nMe gustaría obtener más información sobre las clases de pilates.\n\nGracias."
+  );
 
   emailButton.addEventListener("click", function () {
-    window.open("mailto:" + emailAddress);
+    window.open(`mailto:${emailAddress}?subject=${subject}&body=${body}`);
   });
 });
