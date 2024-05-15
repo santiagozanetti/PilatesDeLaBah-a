a46e66a088851dd8c73f274776483fefa36f7ec6
Santiago <santizanetti117@gmail.com>
2024-05-15 19:34:24 -0300

    cambios

--- js/schedule.js
+++ js/schedule.js
@@ -2,15 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const scheduleBody = document.getElementById("schedule-body");
 
   const schedule = {
+    turnoCero: ["-", "-", "7:00", "-", "-", "-"],
     primerTurno: ["8:00", "8:30", "8:00", "8:30", "8:00", "-"],
     segundoTurno: ["9:00", "9:30", "9:00", "9:30", "9:00", "9:00"],
     tercerTurno: ["10:00", "10:30", "10:00", "10:30", "10:00", "10:00"],
     cuartoTurno: ["11:00", "11:30", "11:00", "11:30", "11:00", "11:00"],
     mediodia: ["-", "-", "-", "-", "-", "-"],
-    primerTurnoTarde: ["16:00", "16:00", "16:00", "16:00"],
-    segundoTurnoTarde: ["17:00", "17:00", "17:00", "17:00"],
-    tercerTurnoTarde: ["18:00", "18:00", "18:00", "18:00"],
-    ultimoTurnoTarde: ["19:00", "19:00", "19:00", "19:00"],
+    turnoTardeCero: ["15:00", "-", "15:00", "-", "-"],
+    primerTurnoTarde: ["16:00", "16:00", "16:00", "16:00", "16:00"],
+    segundoTurnoTarde: ["17:00", "17:00", "17:00", "17:00", "17:00"],
+    tercerTurnoTarde: ["18:00", "18:00", "18:00", "18:00", "18:00"],
+    ultimoTurnoTarde: ["19:00", "19:00", "19:00", "19:00", "19:00"],
   };
 
   Object.keys(schedule).forEach((day) => {
